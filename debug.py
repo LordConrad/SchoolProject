0da27c39175ecc87587319bc950f3bacc4a617e8
Tomáš Maloušek <D24719@oa-opava.cz>
2025-09-18 08:03:46 +0200

    Přidání funkce pro chat a cyklu pro uživatelský vstup do souboru main.py

--- debug.py
+++
@@ -1,4 +0,0 @@
-def chat(mes):
-    print(f"[LOG] {mes}")
-
-chat(mes=input("Chat > "))
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
- 
+def chat(mes):
+    if mes == "!exit":
+        return 1
+    else:
+        print(f"[User] {mes} \n")
+    return 0
+    
+
+while True:
+    if chat(mes=input("Chat > ")) == 1:
+        print("Program has ended")
+        break
+    
