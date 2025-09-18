cbabc3cce38003aea919c26f27c767f97b37e869
Tomáš Maloušek <D24719@oa-opava.cz>
2025-09-18 08:26:11 +0200

    Oprava příkazů pro aktivaci a deaktivaci časových značek v chatu a zjednodušení logiky tisku zpráv

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import time
 
+timestmps = False
+
 def chat(mes):
     global timestmps
     if mes == "!exit":
         return 1
-    elif mes == "tm-true":
+    elif mes == "!tm-true":
         timestmps = True
+        print("> Time stamps activated")
+        return 0
+    elif mes == "!tm-false":
+        timestmps = False
+        print("> Time stamps deactivated")
     else:
-        if timestmps:
-            print(f"[{time.localtime()}][User] {mes} \n")
-        else:
-            timestmps = False
+        print(f"[{time.ctime()}][User] {mes}  \n" if timestmps else print(f"[User] {mes} \n"))
             
+
 while True:
     if chat(mes=input("Chat > ")) == 1:
         print("Program has ended")
-        break
-while True:
-    if chat(mes=input("Chat > ")) == 1:
-        print("Program has ended")
-        break
+        quit()
     
